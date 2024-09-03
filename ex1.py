454910da63dbad5fb947926ae915a1a76f09a4ac
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-09-02 23:03:57 -0300

    ex1

--- ex1.py
+++ ex1.py
@@ -1,2 +1,9 @@
 # Dados 3 lados de um triângulo, calcule e mostre seu perímetro (soma dos lados).
 
+ladoA = int(input("Informe o lado A do triângulo: "))
+ladoB = int(input("Informe o lado B do triângulo: "))
+ladoC = int(input("Informe o lado C do triângulo: "))
+
+perimetro = ladoA + ladoB + ladoC
+
+print(f"o perímetro do triângulo é {perimetro}")
